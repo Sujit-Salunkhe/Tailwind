90c225a540141bc6faa10f2d65fb1e7459893dce
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2024-04-05 21:09:10 +0530

    'firstCommit'

---
+++ tailwind/april2024/week1/day5/my-vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [count,setCount] = useState(0)
+  const handleClick= () => {
+    setCount(count + 1)
+  }
+  return (
+    <>
+    <button onClick={handleClick}>{count}</button>
+  
+    </>
+  )
+}
+
+export default App
